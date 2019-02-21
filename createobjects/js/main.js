1e5853e10d250a5378d24f91b5e61d66578cc415
visualstudio <visualstudio@230-PC11>
2019-02-21 08:51:38 -0500

    //c0740950

--- createobjects/js/main.js
+++ createobjects/js/main.js
@@ -1,1 +1,11 @@
 ﻿// Your code here!
+var bird = {
+    genus: "corvus",
+    species: "corvax",
+    commonName: "raven",
+    callType: "squeaky",
+    quote: "Nevermore",
+    maxOffSpring: 5,
+    noisy: true,
+    deadly: false
+};
